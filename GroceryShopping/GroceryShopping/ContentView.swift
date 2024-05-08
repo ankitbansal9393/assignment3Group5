d6b083789174711746d9592557177f2108c8947b
Jade107027 <jdown1027@gmail.com>
2024-05-08 22:44:21 +1000

    updated views

--- GroceryShopping/GroceryShopping/ContentView.swift
+++ GroceryShopping/GroceryShopping/ContentView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-
+/*
 struct ContentView: View {
     @ObservedObject var viewModel = GroceryListViewModel()
     
@@ -36,4 +36,46 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}*/
+
+struct ContentView: View {
+    @State private var isActive = false
+
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color(hex: "#8CBDB9")
+                    .edgesIgnoringSafeArea(.all)
+                
+                
+                VStack {
+                    Image("logo2")
+                        .resizable()
+                        .frame(width: 300, height: 300)
+                        
+                }
+                
+                
+                
+                NavigationLink(destination: WelcomeView(),
+                                               isActive: $isActive,
+                                               label: {  })
+            
+            }
+            .onAppear(perform: {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                            self.isActive = true
+                        }
+            })
+        }
+        .background(Color.mainMintColor)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarHidden(true)
+       
+    }
 }
+
+#Preview {
+    ContentView()
+        .onAppear(perform: UIApplication.shared.addTapGestureRecognizer)
